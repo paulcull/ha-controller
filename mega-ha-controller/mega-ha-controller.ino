141b27860cd78e67d4a51a1f28ca93ed4b2dd373
Paul Cullender <paul@pkhome.co.uk>
2017-11-04 06:50:31 +0000

    last fixes
    need to still check the MQTT version

--- mega-ha-controller/mega-ha-controller.ino
+++ mega-ha-controller/mega-ha-controller.ino
@@ -1,14 +1,49 @@
 
-
+// Include Watchdog Controller
 #include <avr/wdt.h>
 
+// Include Network Controllers
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+
+// Include JSON library
+#include <ArduinoJson.h>
+
+// Include MQTT Controller
+#include <PubSubClient.h>
+
 // Controller Name
-const String controllerName = "HA-Controller1";
+const char* controllerName = "HA-Controller1";
+
+// Network Info
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x01 };
+//byte ip[] = { 192, 168, 1, 232 }; // use DHCP
+byte server[] = { 64, 233, 187, 99 }; // Google
+
+// MQTT Info
+//const char* mqtt_server = "openHABianPine64.local";
+const char* mqtt_server = "Pauls-MacBook-Pro.local";
+const int mqtt_port = 1883;
+//const char* mqtt_channel = "home/bus/";
+const char* mqtt_channel_hb = "/home/bus/controller/heartbeat";
+const char* mqtt_channel_pub = "/home/bus/controller/";
+const char* mqtt_channel_sub = "/home/bus/controller/";
+char message_buff[100]; //callback message buffer for 100 chars
+StaticJsonBuffer<200> jsonBuffer;
+
+// Create Ethernet Client
+EthernetClient netclient;
+
+// Create MQTT Client
+PubSubClient mqttclient(netclient);
+
 
 // button PINS
 #define NUM_BUTTONS 8
 const int button1 = 23;// was 23 - pushButton on 49
-const int button2 = 49; //was 25
+const int button2 = 25; //was 25
 const int button3 = 27;
 const int button4 = 29;
 const int button5 = 31;
@@ -78,25 +113,17 @@ const int relayState7 = 0;
 const int relayState8 = 0;
 int relayStates[] = {relayState1,relayState2,relayState3,relayState4,relayState5,relayState6,relayState7,relayState8};
 
-//// FUNCTIONS ////
-// NONE
-
-
 
 void setup()
 {
 
   // start the serial library:
   Serial.begin(19200);
-  Serial.println("Programme started on " + controllerName + "...");
-
-  // Start watchdog
-  wdt_enable(WDTO_1S);
-  Serial.println("Watchdog started at 1 seconds");
+  Serial.println("Programme started on " + String(controllerName) + "...");
 
 
   //// Set Pins START ////
-
+  Serial.println("Setting up I/O Pins...");
   for (int i = 0; i < NUM_RELAYS; i++) {
     String msg = "setting button " + String(i);
     String msg2 = msg + " for pin " + String(buttons[i]);
@@ -108,6 +135,55 @@ void setup()
     pinMode(relays[i], OUTPUT);
     digitalWrite(relays[i],RELAY_OFF);
   }
+
+  
+  //// NETWORK setup START ////
+  // start the Ethernet connection:
+  Serial.println("Getting IP Address...");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    for(;;)
+      ;
+  }
+  Serial.print("Got IP address from DHCP server: ");
+  // print your local IP address:
+  Serial.println(Ethernet.localIP());
+    
+  delay(1000); // wait a second and test
+
+  Serial.print("testing connection to internet...");
+
+  if (netclient.connect(server, 80)) {
+    Serial.println("connected");
+    netclient.println("GET /search?q=arduino HTTP/1.0");
+    netclient.println();
+  } else {
+    Serial.println("connection failed");
+  }
+  //// NETWORK setup END ////
+
+  //// MQTT setup START ////
+  //now connect to the mqtt server
+  Serial.print("Connecting to MQTT server...");
+  Serial.println(mqtt_server);
+  mqttclient.setServer(mqtt_server, mqtt_port);
+  mqttclient.connect(controllerName);
+  Serial.print("mqtt connected status...");
+  Serial.println(mqttclient.connected());
+  Serial.print("mqtt state...");
+  Serial.println(mqttclient.state());
+  reconnect();
+//  Serial.println("Going to put test on MQTT:" + String(mqtt_channel_hb));
+  Serial.print("sending mqtt msg...");
+  Serial.println(mqttclient.publish(mqtt_channel_hb,"I'm alive"));
+  mqttclient.setCallback(callback);
+  //// MQTT setup END ////
+
+
+  // Start watchdog
+  wdt_enable(WDTO_8S);
+  Serial.println("Watchdog started at 8 seconds...");
 
 }
 
@@ -126,9 +202,7 @@ void loop()
         Serial.println("Button " + String(i) + " pressed for " + String(buttonCounters[i]) + " time.");
         //Now toggle the relay state for the corresponding button
         relayStates[i] = !relayStates[i];
-        //Set Relay based on new state
         Serial.println("Setting relay " + String(i) + " to " + String(relayStates[i]));
-        digitalWrite(relays[i],relayStates[i]);
       } else {
         Serial.println("Button " + String(i) + " released.");
       }
@@ -136,12 +210,69 @@ void loop()
     // save the current state as the last state,
     //for next time through the loop
     buttonLastStates[i] = buttonStates[i];
-    
+
+    //Set Relay based on stored state
+    //This allows the state to be set by something else like the mqtt callback controller
+    digitalWrite(relays[i],relayStates[i]);
+
     // Delay a little bit to avoid bouncing
     delay(5);
   }
   
 }
 
+
+//// FUNCTIONS ////
+
+//// reconnect to the MQTT server
+void reconnect() {
+  Serial.println("Checking MQTT connection...");
+  // Loop until we're reconnected
+  while (!mqttclient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttclient.connect(controllerName)) {
+      Serial.println("connected... Subscribing to " + String(mqtt_channel_sub));
+      mqttclient.publish(mqtt_channel_hb,"I'm alive"); 
+      // ... and subscribe to topic
+      mqttclient.subscribe(mqtt_channel_sub);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttclient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+//// MQTT message processor
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.println("] ");
+  Serial.println("Length: " + String(length,DEC));
+
+  for (unsigned int i = 0; i < length; i++) {
+    char receivedChar = (char)payload[i];
+    Serial.print(receivedChar);
+    message_buff[i] = payload[i];
+  }
+  message_buff[length+1] = '\0';
+  String msgString = String(message_buff);
+  
+  Serial.println("Payload: " + msgString);
+
+  // {"relay":1,"state":1} - relays are 0-7 / state is 0 or 1
+  JsonObject& root = jsonBuffer.parseObject(msgString);
+  int relay = root["relay"];
+  int state = root["state"];
+
+  relayStates[relay] = state;
+  //this should get picked up in the next loop, as the relay state register controls the relay
+  
+}
+ 
+//// END FUNCTIONS ////
 
 
